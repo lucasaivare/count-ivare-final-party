92e83b3196d228adbe270d3e15cd0b443272bdb5
lucasaivare <lucasa@ivare.com.br>
2023-12-07 12:58:55 -0300

    Update ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Festinha',
   description: 'Festinha de final de ano da ivare',
+  icons: ['yoshi.gif']
 }
 
 export default function RootLayout({
